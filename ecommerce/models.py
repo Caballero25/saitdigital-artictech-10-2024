1fe05b6e91ff90a573b40a016d1dbd37f9fb1bca
David Caballero <davidcame124@gmail.com>
2024-10-31 18:49:33 -0500

    Products queryset on template

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    disponible = models.BooleanField(default=True)
+    precio = models.FloatField()
+    imagen_izquierda = models.ImageField(default=None)
+    imagen_derecha_1 = models.ImageField(default=None)
+    imagen_derecha_2 = models.ImageField(default=None)
+    en_oferta = models.BooleanField(default=False)
+    precio_oferta = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Producto
 
 # Create your views here.
 
 def products_template(request):
-    return render(request, 'ecommerce/products.html')
+    queryset = Producto.objects.filter(disponible=True)  # Obtén el queryset
+    paginator = Paginator(queryset, 10)  # 10 elementos por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'ecommerce/products.html', {'page_obj': page_obj})
