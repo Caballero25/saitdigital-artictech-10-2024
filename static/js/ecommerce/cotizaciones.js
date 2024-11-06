c6ca783ce9c45ea8a410905226208a03e43a920a
David Caballero <davidcame124@gmail.com>
2024-11-06 09:54:34 -0500

    Update location to window open on cotizaciones.js

--- static/js/ecommerce/cotizaciones.js
+++ static/js/ecommerce/cotizaciones.js
@@ -9,6 +9,6 @@ function cotizar_whatsaap(producto) {
     var phoneNumber = "593962636961"
     var whatsAppUrl = `https://wa.me/${phoneNumber}?text=Me%20interesa%20recibir%20una%20cotización%20del%20producto%20*${producto}*`;
     //location.href = whatsAppUrl
-    //window.open(whatsAppUrl)
-    window.location.href = whatsAppUrl
+    window.open(whatsAppUrl)
+    //window.location.href = whatsAppUrl
 }
