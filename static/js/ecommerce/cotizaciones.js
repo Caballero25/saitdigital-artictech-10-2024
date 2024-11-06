9e47959c7f13e60f3e43dea3a8a3763ee5d967b9
developer <davidcame124@gmail.com>
2024-11-05 20:47:11 -0500

    redirection to ws and email

---
+++ static/js/ecommerce/cotizaciones.js
@@ -0,0 +1,14 @@
+function cotizar_email(producto) {
+    var mailto = "saittdocuments@gmail.com"
+    var gmailUrl = `mailto:${mailto}?subject=Cotización%20-%20ArtItTech&body=Me%20interesa%20cotizar%20el%20producto%20${producto}.`
+    window.open(gmailUrl)
+}
+
+
+function cotizar_whatsaap(producto) {
+    var phoneNumber = "593962636961"
+    var whatsAppUrl = `https://wa.me/${phoneNumber}?text=Me%20interesa%20recibir%20una%20cotización%20del%20producto%20*${producto}*`;
+    //location.href = whatsAppUrl
+    //window.open(whatsAppUrl)
+    window.location.href = whatsAppUrl
+}
